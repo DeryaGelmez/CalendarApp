2bddfb3c625cecc1af1e7654dd45e0503374245f
derya <derya@DERYA>
2023-09-27 18:44:03 +0300

    Add project files.

---
+++ Calendar/Program.cs
@@ -0,0 +1,47 @@
+﻿using Calendar.Entities;
+using System.Runtime.InteropServices;
+
+namespace Calendar
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calendar App");
+            Meeting meeting1 = new()
+            {
+
+                Title = "YetGen Jump & Akbank Backend Planlama Toplantısı",
+                Details = new List<string>() { "Katılımcıların alım süreçleri konuşulacak, Akbank'tan gelen mail konuşulacak,bütçe planlaması konuşulacak." },
+                StartTime = new DateTime(2023, 09, 18, 19, 00, 00),
+                FinishTime = new DateTime(2023, 09, 18, 20, 00, 00),
+                Guests = new() { "hakan@deneme.com", "mehmet@deneme.com", "ugur@deneme.com" },
+            };
+
+            Todo todo1 = new()
+            {
+
+                Title = "Katılımcı geri bildirimlerini teslim et.",
+                Details = new List<string>() { "Değerlendirme sonuçlarının anonimleştirilmesi ve grafik oluşturulması" },
+                StartTime = new DateTime(2023, 09, 20, 21, 00, 00),
+                FinishTime = new DateTime(2023, 09, 20, 22, 00, 00),
+                Importance = "High Priority",
+
+            };
+
+            meeting1.GetNotification();
+            todo1.GetNotification();
+
+
+
+
+
+        }
+    }
+}
+
+
+
+
+
